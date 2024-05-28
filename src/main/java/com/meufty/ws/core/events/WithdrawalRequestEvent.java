9600cb71a41de8426486c05d5ef19d873499024d
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-28 08:05:26 -0400

    implementation of the withdrawal request event

--- src/main/java/com/meufty/ws/core/events/WithdrawalRequestEvent.java
+++ src/main/java/com/meufty/ws/core/events/WithdrawalRequestEvent.java
@@ -1,4 +1,42 @@
 package com.meufty.ws.core.events;
 
+import java.math.BigDecimal;
+
 public class WithdrawalRequestEvent {
+    private String senderId;
+    private String recepientId;
+    private BigDecimal amount;
+
+    public WithdrawalRequestEvent() {
+    }
+
+    public WithdrawalRequestEvent(String senderId, String recepientId, BigDecimal amount) {
+        this.senderId = senderId;
+        this.recepientId = recepientId;
+        this.amount = amount;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getRecepientId() {
+        return recepientId;
+    }
+
+    public void setRecepientId(String recepientId) {
+        this.recepientId = recepientId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }
