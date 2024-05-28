d4f9ec29ee2551c74d448da3f7dc4ae1c909bd5c
I M Ndangoh <37620979+ndangohmofor@users.noreply.github.com>
2024-05-28 07:55:42 -0400

    implementation for deposit request event

--- src/main/java/com/meufty/ws/core/events/DepositRequestEvent.java
+++ src/main/java/com/meufty/ws/core/events/DepositRequestEvent.java
@@ -1,2 +1,42 @@
-package com.meufty.ws.core.events;public class DepositRequestEvent {
+package com.meufty.ws.core.events;
+
+import java.math.BigDecimal;
+
+public class DepositRequestEvent {
+    private String senderId;
+    private String recepientId;
+    private BigDecimal amount;
+
+    public DepositRequestEvent() {
+    }
+
+    public DepositRequestEvent(String senderId, String recepientId, BigDecimal amount) {
+        this.senderId = senderId;
+        this.recepientId = recepientId;
+        this.amount = amount;
+    }
+
+    public String getSenderId() {
+        return senderId;
+    }
+
+    public void setSenderId(String senderId) {
+        this.senderId = senderId;
+    }
+
+    public String getRecepientId() {
+        return recepientId;
+    }
+
+    public void setRecepientId(String recepientId) {
+        this.recepientId = recepientId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
 }
